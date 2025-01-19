a902539fda883cc91a433256ecaef15a91233193
Void Drifter (VoltacceptYT) <voltaccept@gmail.com>
2025-01-19 11:56:57 -0500

    Update01

--- commands/serverip.js
+++ commands/serverip.js
@@ -1,11 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const addressEmbed = {
+	color: 0x3AE474,
+	title: 'PLAY.VOLTACCEPT.COM',
+	description: 'Join us today and become part of a vibrant and friendly community. Whether you\'re a seasoned builder, an ambitious explorer, or a fierce competitor, VoltaMC has something for everyone. Gear up, log in, and let the adventures begin!',
+	timestamp: new Date().toISOString(),
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setName('server-ip')
+    .setDescription('Replies With VoltaMC\'s Server IP'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    await interaction.reply({embeds: [addressEmbed]});
   },
 };
 
